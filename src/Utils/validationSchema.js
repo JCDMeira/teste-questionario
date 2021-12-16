b891208ecb4b6770e22d72bdd79c9a9208f6e336
Jean Meira <jean.meira10@hotmail.com>
2021-12-16 14:21:02 -0300

    Add: error message for is required

--- src/Utils/validationSchema.js
+++ src/Utils/validationSchema.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 export default Yup.object().shape({
   questionaryNumber: Yup.number()
-    .min(1, 'Minimum number of questions accepted is 1 ')
-    .max(50, 'The maximum number of questions accepted is 50 ')
-    .required(),
+    .min(1, 'Minimum number of questions accepted is 1')
+    .max(50, 'The maximum number of questions accepted is 50')
+    .required('This is a required field '),
 });
